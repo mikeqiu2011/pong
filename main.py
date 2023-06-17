e83b26891d88190228b259c4dc04e447d285b5a9
mikeqiu2011 <mikeqiu2011@163.com>
2023-06-17 10:39:43 +0800

    left paddle done

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ screen.title('Pong')
 screen.tracer(0)
 screen.listen()
 
-r_paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.onkey(fun=r_paddle.go_up, key='Up')
 screen.onkey(fun=r_paddle.go_down, key='Down')
+
+screen.onkey(fun=l_paddle.go_up, key='w')
+screen.onkey(fun=l_paddle.go_down, key='s')
 
 game_is_on = True
 
--- paddle.py
+++ paddle.py
@@ -2,12 +2,12 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape('square')
         self.shapesize(stretch_len=1, stretch_wid=5)
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
         self.color('white')
 
     def go_up(self):
